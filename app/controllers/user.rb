290af736bc8c8b8d96c772c366f88023910197cd
Stephen Roth <stephenroth.sr@gmail.com>
2014-05-29 18:04:32 -0400

    database interaction in games controller

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -2,14 +2,24 @@
 get '/users/profile' do
 	@user = User.find(session[:user_id])
 	@users_created_games = @user.games
-	##### Complicated database query. I hope it's correct.
-	users_played_games = Drawing.all.find_by_user_id(session[:user_id])
-	users_played_games << Description.all.find_by_user_id(session[:user_id])
-	@user_played_game_ids = []
 
-	users_played_games.each do |game_part|
-		@user_played_game_ids << game_part.game_id
-	end
+	##### I know the statements are probably wrong, but one of these two options should work
+
+# 1
+	@users_played_games = Drawing.where(user_id: session[:user_id])
+	@users_played_games << Description.where(user_id: session[:user_id])
+	@users_played_games.flatten!
+
+	##### Complicated database query. I hope it's correct.
+
+# 2
+	# users_played_games = Drawing.all.find_by_user_id(session[:user_id])
+	# users_played_games << Description.all.find_by_user_id(session[:user_id])
+	# @user_played_game_ids = []
+
+	# users_played_games.each do |game_part|
+	# 	@user_played_game_ids << game_part.game_id
+	# end
 # Default user page. Has links to see in progress games, games created by user, games played by user.
   erb :profile_page
 end
