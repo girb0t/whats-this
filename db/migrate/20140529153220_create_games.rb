1f7d7ca7c015648d25cdff709e9ae760e8c0a88b
Stephen Roth <stephenroth.sr@gmail.com>
2014-05-29 17:50:49 -0400

    updated games table - now includes :is_completed column

--- db/migrate/20140529153220_create_games.rb
+++ db/migrate/20140529153220_create_games.rb
@@ -2,6 +2,7 @@ class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
       t.belongs_to :user
+      t.boolean :is_complete, default: false
 
       t.timestamps
     end
