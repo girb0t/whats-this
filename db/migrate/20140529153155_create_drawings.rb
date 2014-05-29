ab1a6ddba44f93a817e4bc188c26fb4c08f696de
Pei <peixiong.liu@gmail.com>
2014-05-29 16:05:15 -0400

    migrations done

--- db/migrate/20140529153155_create_drawings.rb
+++ db/migrate/20140529153155_create_drawings.rb
@@ -1,4 +1,12 @@
 class CreateDrawings < ActiveRecord::Migration
   def change
+    create_table :drawings do |t|
+      t.belongs_to :user
+      t.belongs_to :game
+      t.belongs_to :description
+      t.string :svg_file
+
+      t.timestamps
+    end
   end
 end
--- db/migrate/20140529153207_create_descriptions.rb
+++ db/migrate/20140529153207_create_descriptions.rb
@@ -1,4 +1,12 @@
 class CreateDescriptions < ActiveRecord::Migration
   def change
+    create_table :descriptions do |t|
+      t.belongs_to :user
+      t.belongs_to :game
+      t.belongs_to :drawing
+      t.string :body
+
+      t.timestamps
+    end
   end
 end
