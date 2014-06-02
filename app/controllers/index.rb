f0af726de6610f68cb0bfa5547c88f76e313c17e
Stephen Roth <stephenroth.sr@gmail.com>
2014-06-02 19:40:13 -0400

    updated error messages and associated views/controllers

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,18 +5,14 @@ get '/' do
 end
 
 post '/signup' do
-  # CREATE NEW USER
   user = User.new(username: params[:username], password: params[:password], password_confirmation: params[:password])
   if user.save  
     session[:user_id] = user.id
     session[:username] = user.username
     redirect '/'
   else
-    # Error message should check to see if user exists
-    @error = "There was a problem creating a new user. "
-    @error += "The username was probably already taken. "
-    @error += "More useful error messages in the works."
-    erb :error 
+    session[:last_error] = "There was a problem creating a new user."
+    redirect '/'
   end
 end
 
@@ -27,9 +23,8 @@ post '/login' do
     session[:username] = user.username
     redirect '/'
   else
-    @error = "There was a problem logging in. Please try again. "
-    @error += "More useful error messages in the works."
-    erb :error
+    session[:last_error] = "Problem logging in.  Please check your username and password and try again."
+    redirect '/'
   end
 end
 
